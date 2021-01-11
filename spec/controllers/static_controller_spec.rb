638d5c78472ee7db6075dc63aed33b1c56999935
James Donovan <jameswilliamdonovan@gmail.com>
2021-01-11 01:51:04 +0000

    Automatically backed up by Learn

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe StaticController, type: :controller do
-  describe 'navigate to /hello_world page' do
-    it 'says hello world' do
-      visit '/hello_world'
-      expect(page).to have_css('h1', text: 'Hello World')
-    end
-  end
+  
 end
